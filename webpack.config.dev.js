6e6937a645523192520cb8ec24dd5f2cf1d9b15b
Adriano Tadao <drianotadao@gmail.com>
2016-05-22 15:30:56 -0300

    Class 2

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -21,12 +21,13 @@ module.exports = {
     // js
     {
       test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'client')
+      loader: 'babel',
+      include: path.join(__dirname, 'client'),
+      query: { presets: ['es2015', 'react'] }
     },
     // CSS
-    { 
-      test: /\.styl$/, 
+    {
+      test: /\.styl$/,
       include: path.join(__dirname, 'client'),
       loader: 'style-loader!css-loader!stylus-loader'
     }
